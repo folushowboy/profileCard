b556dc69613b398c9842ddfd0efbe95d15261253
toluolpe <odusinat8@gmail.com>
2025-10-22 14:46:19 +0100

    update

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,35 +1,29 @@
-// alert("connected")
 
 const form = document.querySelector("form");
 const email = document.getElementById("email");
+const emailError = document.getElementById("erroremailtext");
 const message = document.getElementById("message");
-const errorMsg = document.getElementById("error-message");
-const errorEmail = document.getElementById("error-email");
+const messageError = document.getElementById("errormsgtext");
 const successMsg = document.getElementById("success");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  showError();
 });
 
 email.addEventListener("input", (e) => {
   if (email.validity.valid) {
-    // If the email is valid (matches name@example.com)
-    errorEmail.textContent = "";
-    errorEmail.className = "error";
+    emailError.textContent = "";
+    emailError.className = "error";
   } else {
-    // If the email is invalid or empty
     showError();
   }
 });
+
 message.addEventListener("input", (e) => {
   if (message.validity.valid) {
-    // If the email is valid (matches name@example.com)
-    errorMsg.textContent = "";
-    errorMsg.className = "error";
+    messageError.textContent = "";
+    messageError.className = "error";
   } else {
-    // If the email is invalid or empty
     showError();
   }
 });
@@ -37,27 +31,29 @@ message.addEventListener("input", (e) => {
 document.getElementById("submit").addEventListener("click", showError);
 
 function showError() {
-  if (message.validity.valid) {
-    errorMsg.textContent = "";
-    errorMsg.className = "error";
-    successMsg.textContent = "Successfully Submitted!";
+  if (email.validity.valid && message.validity.valid) {
+    emailError.textContent = "";
+    messageError.textContent = "";
+    successMsg.textContent = "Form submitted successfully!";
+
+    form.reset();
+
+    setTimeout(() => {
+      successMsg.textContent = "";
+    }, 1000);
+
     return;
   } else if (email.validity.valueMissing) {
-    errorMsg.textContent = "You need to enter an email address.";
+    emailError.textContent = "You need to enter an e-mail address.";
   } else if (email.validity.typeMismatch) {
-    errorMsg.textContent = "Email must be valid (name@example.com).";
+    emailError.textContent = "Email must be valid (name@example.com).";
   } else if (message.validity.valueMissing) {
-    errorMsg.textContent = `You need to enter a message (Message should be at least ${message.minLength} characters; you entered ${message.value.length}).`;
-    return;
+    messageError.textContent = "You need to enter a message.";
+  } else if (message.validity.tooShort) {
+    messageError.textContent = `Message should be at least ${message.minLength} characters; you entered ${message.value.length}.`;
   }
 
   // Show error message styling
-  errorMsg.className = "error active";
-  errorEmail.style.color = "red";
-  errorMsg.style.color = "red";
-}
-
-function clearMsg() {
-  document.querySelector("form").reset();
-  successMsg.textContent = "";
+  emailError.style.color = "#ff5252";
+  messageError.style.color = "#ff5252";
 }
